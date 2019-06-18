1f94781fa46d385bbb119fd8fe7888dd9ef1cebf
ladodoche <d.alayrangues@gmail.com>
2019-06-18 22:28:55 +0200

    route get all user, group, bar & brewery

--- configs/Routes/general.js
+++ configs/Routes/general.js
@@ -209,4 +209,114 @@ generalRouter.get('/research/:data', function(req, res) {
   ]);
 });
 
+/**
+* @api {get} getallusergroupbarbrewery getall user, group, bar or brewery
+* @apiGroup Generals
+* @apiParam {String} data
+* @apiSuccessExample {json} Success
+*  HTTP/1.1 200 Success
+* {
+*    "error": false,
+*    "users": [
+*        {
+*            "admin": false,
+*            "id": 1,
+*            "email": "d.alayrangues@gmail.com",
+*            "name": "dorian",
+*            "surname": "alayrangues",
+*            "birthDate": "1997-05-22T00:00:00.000Z",
+*            "updated_at": "2019-04-14T09:20:46.668Z",
+*            "created_at": "2019-04-14T09:20:46.668Z"
+*        }
+*    ],
+*    "groups": [
+*        {
+*            "id": 1,
+*            "name": "group paris",
+*            "created_at": "2019-04-14T13:42:47.000Z",
+*            "updated_at": "2019-04-14T13:42:47.000Z",
+*            "deleted_at": null,
+*            "admin_id": 1
+*        }
+*    ],
+*    "bars": [
+*        {
+*            "id": 1,
+*            "name": "Le dernier bar avant la fin du monde",
+*            "gpsLatitude": 48,
+*            "gpsLongitude": 2.3461672,
+*            "description": "Coucou",
+*            "webSiteLink": "https://www.facebook.com/?ref=tn_tnmn",
+*            "created_at": "2019-04-14T13:42:47.000Z",
+*            "updated_at": "2019-04-14T13:42:47.000Z",
+*            "deleted_at": null,
+*            "user_id": 1
+*        }
+*    ],
+*    "brewerys": [
+*        {
+*            "id": 1,
+*            "name": "La dernière brasserie avant la fin du monde",
+*            "gpsLatitude": 48,
+*            "gpsLongitude": 2.3461672,
+*            "description": "Coucou",
+*            "webSiteLink": "https://www.facebook.com/?ref=tn_tnmn",
+*            "created_at": "2019-04-14T13:42:47.000Z",
+*            "updated_at": "2019-04-14T13:42:47.000Z",
+*            "deleted_at": null,
+*            "user_id": 1
+*        }
+*    ]
+* }
+* @apiErrorExample {json} Error
+*    HTTP/1.1 500 Internal Server Error
+*    {
+*        "error": true,
+*        "message": message
+*    }
+*/
+generalRouter.get('/getallusergroupbarbrewery', function(req, res) {
+
+  const data = req.params.data;
+
+  asyncLib.waterfall([
+    function(done){
+      UserController.getAll()
+      .then((users) => {
+        done(null, users)
+      })
+      .catch((err) => {
+        return res.status(500).json({"error": true, "message": "Erreur lors de la recherche des utilisateurs"});
+      });
+    },
+    function(users, done){
+      BarController.getAll()
+      .then((bars) => {
+        done(null, users, bars)
+      })
+      .catch((err) => {
+        return res.status(500).json({"error": true, "message": "Erreur lors de la recherche des bars"});
+      });
+    },
+    function(users, bars, done){
+      BreweryController.getAll()
+      .then((brewerys) => {
+        done(null, users, bars, brewerys)
+      })
+      .catch((err) => {
+        return res.status(500).json({"error": true, "message": "Erreur lors de la recherche des brasseries"});
+      });
+    },
+    function(users, bars, brewerys, done){
+      GroupController.getAll()
+      .then((groups) => {
+        return res.status(200).json({"error": false, "users": users, "bars": bars, "brewerys": brewerys, "groups": groups}).end();
+      })
+      .catch((err) => {
+        return res.status(500).json({"error": true, "message": "Erreur lors de la recherche des groupes"});
+      });
+    }
+  ]);
+});
+
 module.exports = generalRouter;
